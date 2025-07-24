f1c6521ff5fe7510368380728a0cc27ffdd9c8e1
sam43453 <heresam4354@gmail.com>
2025-07-24 11:29:45 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Insert your Telegram bot token here
+# The token is given when you create a new bot via BotFather in Telegram.
+TOKEN = '7503934696:AAFVJHA-IcyKqk7gSzZccyRa5vMRtu_Zkoc'
+
+# MongoDB URI for connecting to a MongoDB database.
+# This is a connection string that allows your bot to access a MongoDB cluster.
+MONGO_URI = 'mongodb+srv://sushil:sushil@lgcyalex8565.9q6nj.mongodb.net'
+
+# Enter your Admin's Chat's ID here, Get it from @missRose_bot by typing /id
+# The admin IDs refer to the Telegram user IDs who have administrative access to the bot and its operations.
+ADMIN_IDS = [1419969308, 9876543210]  # Replace with the actual user IDs of your bot admins.
